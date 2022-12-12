bd0429b4bd865f32abc2df9eae919b91423e347e
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-12 19:27:10 +0530

    test

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -5,3 +5,31 @@ variable sample {
 output "sample" {
   value = var.sample
 }
+
+variable "ex-list" {
+    default = [
+        "Cloud",
+        "Devops",
+        100,
+        false,
+        "Verma"
+    ]
+}
+
+output "ex-list" {
+    value = "${var.ex-list[0]} ${var.ex-list[1]}"
+}
+
+variable "ex-list-output" {
+    default = {
+        duration = 105
+        timing   = "0730AMIST"
+        trainer  = "Mannu"
+    }
+  
+}
+
+output "ex-list-output" {
+    value = "${var.ex-list-output["duration"]} ${var.ex-list-output["trainer"]}" 
+  
+}
