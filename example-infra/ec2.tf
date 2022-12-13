7702569c06422aafd1ecbdbd0f5d847c7b302609
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-13 22:21:15 +0530

    test

---
+++ example-infra/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web" {
+    ami = ""
+    instance_type = "t2.micro"
+
+    tags = {
+        Name =  "Hello World"
+    }
+  
+}
+
+output "private_dns" {
+    value = "aws_instance.web.private_dns"
+  
+}
