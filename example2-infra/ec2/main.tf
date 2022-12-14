cbfae23fb7bc5c23d65df8edf59413e576a60daa
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-14 14:50:11 +0530

    test

---
+++ example2-infra/ec2/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+    ami = "ami-00ff427d936335825"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [var.sg]
+
+    tags = {
+        Name =  "Hello World"
+    }
+  
+}
+
+variable "sg" {}
+
+output "public_ip" {
+    value = "aws_instance.web.public_ip"
+  
+}
