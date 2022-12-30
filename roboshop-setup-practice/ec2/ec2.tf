7885283c2cf831264c8fcddae7506072ae6d4d7a
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-30 13:33:07 +0530

    test

--- roboshop-setup-practice/ec2/ec2.tf
+++ roboshop-setup-practice/ec2/ec2.tf
@@ -2,13 +2,13 @@ resource "aws_instance" "web" {
     ami = "ami-00ff427d936335825"
     instance_type = "t2.micro"
     vpc_security_group_ids = [var.sg]
-}
+
 
     tags = {
         Name =  "Hello World"
     }
   
-
+}
 
 variable "sg" {}
 
