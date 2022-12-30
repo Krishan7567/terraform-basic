71feca50c18e44c4e39c5a3a047f34ff3fda3ec3
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-30 12:11:58 +0530

    test

--- roboshop-setup-practice/ec2/ec2.tf
+++ roboshop-setup-practice/ec2/ec2.tf
@@ -1,9 +1,12 @@
 # Creates EC2 Instance 
 resource "aws_instance" "app" {
-  ami              = "ami-00ff427d936335825"
-  instance_type    = "t2.micro"
+  ami                     = "ami-00ff427d936335825"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [var.sg]
 
   tags = {
     Name = "my-app"
   }
 }
+
+variable "sg" {}
