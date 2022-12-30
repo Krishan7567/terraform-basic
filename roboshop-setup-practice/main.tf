c256c7693adb7c27ecb13e2e584fa521c1404c0d
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-30 13:44:12 +0530

    test

--- roboshop-setup-practice/main.tf
+++ roboshop-setup-practice/main.tf
@@ -1,12 +1,13 @@
 module "ec2" {
-    source  = "./ec2"
-    sg      = module.sg.sgid
+    source = "./ec2"
+    sg     =  module.sg.sgid
 }
 
 module "sg" {
     source = "./sg"
 }
 
-output "public_ip" {
-    value = module.ec2.public_ip
+# Taking the output to this root module from the sg sub-module 
+output  "public_ip" {
+    value  = module.ec2.public_ip
 }
--- roboshop-setup-practice/sg/sg.tf
+++ roboshop-setup-practice/sg/sg.tf
@@ -1,27 +1,38 @@
-resource "aws_security_group" "allow_ssh" {
-    name        = "allow_ssh"
-    description = "Allow SSH inboud trffic"
-    
-        ingress = {
-      description       = "allow_ssh"
-      from_port         = 22
-      to_port           = 22
-      protocol          = "tcp"
-      cidr_blocks       = ["0.0.0.0/0"]
-    } 
-  
-    egress = {
-      from_port         = 0
-      to_port           = 0
-      protocol          = "-1"
-      cidr_blocks       = ["0.0.0.0/0"]
-      ipv6_cidr_blocks  = ["::/0"]
-    } 
-    tags = {
-        Name = "allow_SSH"
-    }
+# Creates Security Group
+resource "aws_security_group" "allows_ssh" {
+  name        = "allows_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP from Public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allows_SSH"
+  }
 }
 
+# Output 
 output "sgid" {
-    value = "aws_security_group.allow_ssh.id"  
+    value  =  aws_security_group.allows_ssh.id
 }
