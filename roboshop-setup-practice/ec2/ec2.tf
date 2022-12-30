5b831e383e805cd0531e914879afd197409839e0
Krishan Bhatt <krishan.bhatt@hcl.com>
2022-12-30 12:34:32 +0530

    test

--- roboshop-setup-practice/ec2/ec2.tf
+++ roboshop-setup-practice/ec2/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                     = "ami-00ff427d936335825"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  = [var.sg]
+  vpc_security_group_id   = [var.sg]
 
   tags = {
     Name = "my-app"
@@ -10,3 +10,7 @@ resource "aws_instance" "app" {
 }
 
 variable "sg" {}
+output "public_ip" {
+    value = "aws_instance.web.public_ip"
+  
+}
